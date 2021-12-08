e9d3c07f487bacc4cbd3876d088af300682e53d9
ygthns <54602575+ygthns@users.noreply.github.com>
2021-12-07 23:25:31 -0500

    Add files via upload

---
+++ cards/deck.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+// Create a new type of deck
+// which is a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardNumber := []string{"Ace", "Two", "Three", "Four"}
+	for _, suit := range cardSuits {
+		for _, value := range cardNumber {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func toByteSlice(str string) []byte {
+	return []byte(str)
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, toByteSlice(d.toString()), 0666)
+
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	slice := strings.Split(string(bs), ",")
+	return deck(slice)
+}
---
+++ cards/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	//cards := newDeck()
+	//hand, remainingDeck := deal(cards, 5)
+	//hand.print()
+	//remainingDeck.print()
+
+	//	cards := newDeck()
+	//	cards.saveToFile("thirdTry")
+
+	cards := newDeckFromFile("firstTry")
+	cards.print()
+
+}
